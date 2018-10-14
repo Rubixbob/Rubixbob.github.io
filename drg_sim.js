4f1502b539e4225a4ba9c2ac189ffb77d3b43d3b
Rubixbob <julienhans@hotmail.fr>
2018-10-14 20:40:22 +0200

    Added some stuff, see done list

--- drg_sim.js
+++ drg_sim.js
@@ -1,52 +1,52 @@
-actions.weaponskills.forEach(function (ac) {
-	var acId = ac.name.replace("'", "").replace(/\s/g, "");
-	$("#weaponskills").append($("<div></div>").attr({id: acId, class: "action draggable"}).css({"background-image": `url("images/${ac.name}.png")`, "background-repeat": "no-repeat", "background-color": "cyan"}).get());
-});
-
-actions.abilities.forEach(function (ac) {
-	var acId = ac.name.replace("'", "").replace(/\s/g, "");
-	$("#abilities").append($("<div></div>").attr({id: acId, class: "action draggable"}).css({"background-image": `url("images/${ac.name}.png")`, "background-repeat": "no-repeat"}).get());
+actions.forEach(function (ac) {
+    var action = $("<div></div>").attr({name: ac.name, class: `action draggable ${ac.type}`});
+    action.append($("<div></div>").attr("class", "actionImage").css("background-image", `url("images/${ac.name}.png")`));
+    $(`#${ac.group}`).append(action);
 });
 
 (function() {
     var dndHandler = {
-        draggedElement: null, // Propriété pointant vers l'élément en cours de déplacement
+        draggedElement: null,
 
         applyDragEvents: function(element) {
             element.draggable = true;
 
-            var dndHandler = this; // Cette variable est nécessaire pour que l'événement « dragstart » ci-dessous accède facilement au namespace « dndHandler »
+            var dndHandler = this;
 
             element.addEventListener('dragstart', function(e) {
-                dndHandler.draggedElement = e.target; // On sauvegarde l'élément en cours de déplacement
-                e.dataTransfer.setData('text/plain', ''); // Nécessaire pour Firefox
+                var target = e.target;
+                while (target.parentNode.className.indexOf('draggable') != -1) {
+                    target = target.parentNode;
+                }
+                dndHandler.draggedElement = target;
+                e.dataTransfer.setData('text/plain', '');
             });
         },
 
         applyDropEvents: function(dropper) {
             dropper.addEventListener('dragover', function(e) {
-                e.preventDefault(); // On autorise le drop d'éléments
-                this.className = 'dropper drop_hover'; // Et on applique le style adéquat à notre zone de drop quand un élément la survole
+                e.preventDefault();
+                this.className = 'dropper drop_hover';
             });
 
             dropper.addEventListener('dragleave', function() {
-                this.className = 'dropper'; // On revient au style de base lorsque l'élément quitte la zone de drop
+                this.className = 'dropper';
             });
 
-            var dndHandler = this; // Cette variable est nécessaire pour que l'événement « drop » ci-dessous accède facilement au namespace « dndHandler »
+            var dndHandler = this;
 
             dropper.addEventListener('drop', function(e) {
                 var target = e.target,
-                    draggedElement = dndHandler.draggedElement, // Récupération de l'élément concerné
-                    clonedElement = draggedElement.cloneNode(true); // On créé immédiatement le clone de cet élément
+                    draggedElement = dndHandler.draggedElement,
+                    clonedElement = draggedElement.cloneNode(true);
 
-                while (target.className.indexOf('dropper') == -1) { // Cette boucle permet de remonter jusqu'à la zone de drop parente
+                while (target.className.indexOf('dropper') == -1) {
                     target = target.parentNode;
                 }
 
-                target.className = 'dropper'; // Application du style par défaut
+                target.className = 'dropper';
 
-                dndHandler.applyDragEvents(clonedElement); // Nouvelle application des événements qui ont été perdus lors du cloneNode()
+                dndHandler.applyDragEvents(clonedElement);
                 addAction(clonedElement);
                 if (draggedElement.parentNode.id === "rotation")
                 	draggedElement.parentNode.removeChild(draggedElement);
@@ -56,8 +56,11 @@ actions.abilities.forEach(function (ac) {
         applyClickEvents: function(element) {
         	var dndHandler = this;
         	element.addEventListener('click', function(e) {
-        		var target = e.target,
-        			clonedElement = target.cloneNode(true);
+        		var target = e.target;
+                while (target.parentNode.className.indexOf('draggable') != -1) {
+                    target = target.parentNode;
+                }
+                var clonedElement = target.cloneNode(true);
         		dndHandler.applyDragEvents(clonedElement);
         		addAction(clonedElement);
         	});
@@ -69,21 +72,53 @@ actions.abilities.forEach(function (ac) {
 })();
 
 var scale = 60;
+var gcd = 2.5;
 
 function addAction(element, time = 0) {
 	if (time == 0 && $("#rotation").children().length > 0) {
-		time = Number($("#rotation").children().last().attr("time")) + 0.7; // TODO: check GCD + clip
+        var timeGcd = 0;
+        if ($(element).hasClass("Weaponskill") && $("#rotation").children(".Weaponskill").length > 0)
+            timeGcd = (Number($("#rotation").children(".Weaponskill").last().attr("time")) * 100 + gcd * 100) / 100;
+        //animlock
+        var incr = defaultAnimLock;
+        var lastAc = actions.find(ac => $("#rotation").children().last().attr("name") === ac.name);
+        if (lastAc.hasOwnProperty("animLock"))
+            incr = lastAc.animLock;
+        // if (lastAc.name === "Delay") // TODO : Delay
+        //     incr = Number($("#rotation").children(".Weaponskill").last().attr("time"))
+        time = (Number($("#rotation").children().last().attr("time")) * 100 + incr * 100) / 100;
+        time = Math.max(time, timeGcd);
 	}
 	var position = time * scale;
-	$("#rotation").append($(element).attr("time", `${time}`).css({"position": "absolute", "top": `${position}px`}));
+    var offset = 2;
+    if ($(element).hasClass("Ability"))
+        offset += 30;
+    var animLockHeight = scale * defaultAnimLock;
+    curAc = actions.find(ac => $(element).attr("name") === ac.name);
+    if (curAc.hasOwnProperty("animLock"))
+        animLockHeight = scale * curAc.animLock;
+    $("#rotation").append($(element).attr("time", `${time}`).css({"position": "absolute", "top": `${position}px`, "left": `${offset}px`, "height": `${animLockHeight}px`}));
+    addTimeUntil(time + 5);
 }
 
 function addTime(time) {
 	var position = time * scale + document.getElementById("timeline").getBoundingClientRect().top;
-    //$("#timeline").append($(`<div>${time}</div>`).attr("time", `${time}`).css({"position": "absolute", "top": `${position}px`}).get());
     $("#timeline").append($(`<div>${time}</div>`).attr("time", `${time}`).css("height", `${scale}px`).get());
 }
 
-for (i = 0; i < 10; i++) {
-	addTime(i);
+function addTimeUntil(time) {
+    var currentMax = -1;
+    if ($("#timeline").children().length > 0)
+        currentMax = Number($("#timeline").children().last().attr("time"));
+    for (i = currentMax + 1; i <= time; i++) {
+        addTime(i);
+    }
 }
+
+addTimeUntil(10);
+
+$("#clearRotation").click(function(){
+    $("#rotation").empty();
+    $("#timeline").empty();
+    addTimeUntil(10);
+});
