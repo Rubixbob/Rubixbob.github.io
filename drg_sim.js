de9ae7f67ec405cc4e8a6dd3b58a26a0594ba0bf
Rubixbob <julienhans@hotmail.fr>
2018-10-15 23:41:04 +0200

    Added Action Movement

--- drg_sim.js
+++ drg_sim.js
@@ -7,8 +7,9 @@ actions.forEach(function (ac) {
 (function() {
     var dndHandler = {
         draggedElement: null,
+        // clone: null,
 
-        applyDragEvents: function(element) {
+        applyActionsEvents: function(element) {
             element.draggable = true;
 
             var dndHandler = this;
@@ -18,22 +19,64 @@ actions.forEach(function (ac) {
                 while (target.parentNode.className.indexOf('draggable') != -1) {
                     target = target.parentNode;
                 }
-                dndHandler.draggedElement = target;
+                dndHandler.draggedElement = target.cloneNode(true);
+                // dndHandler.clone = dndHandler.draggedElement.cloneNode(true);
                 e.dataTransfer.setData('text/plain', '');
+            });
+
+            element.addEventListener('click', function(e) {
+                var target = e.target;
+                while (target.parentNode.className.indexOf('draggable') != -1) {
+                    target = target.parentNode;
+                }
+                var clonedElement = target.cloneNode(true);
+                dndHandler.applyRotationEvents(clonedElement);
+                addActionAtIndex(clonedElement, $("#rotation").children().length);
             });
         },
 
         applyDropEvents: function(dropper) {
+            var dndHandler = this;
+
             dropper.addEventListener('dragover', function(e) {
                 e.preventDefault();
-                this.className = 'dropper drop_hover';
+                var target = e.target;
+                while (target.className.indexOf('dropper') == -1) {
+                    target = target.parentNode;
+                }
+                // this.className = 'dropper drop_hover';
+                var curIdx = $("#rotation").children().index(dndHandler.draggedElement);
+                if (curIdx === -1)
+                    curIdx = $("#rotation").children().length;
+                addActionAtIndex(dndHandler.draggedElement, curIdx);
+                var time = (e.clientY-target.getBoundingClientRect().top) / scale;
+                var idx = $("#rotation").children().filter(function(index) {
+                    return Number($(this).attr("time")) + getAnimationLock($(this).attr("name")) / 2 <= time
+                        && index != curIdx;
+                }).length;
+                // console.log(idx);
+                if (idx === curIdx)
+                    return;
+                else if (idx > curIdx) {
+                    addActionAtIndex($("#rotation").children().eq(idx), idx-1);
+                    addActionAtIndex(dndHandler.draggedElement, idx);
+                } else {
+                    addActionAtIndex(dndHandler.draggedElement, idx);
+                    addActionAtIndex($("#rotation").children().eq(idx+1), idx+1);
+                }
+                updateRotationAfterIndex(idx + 1);
             });
 
-            dropper.addEventListener('dragleave', function() {
-                this.className = 'dropper';
+            dropper.addEventListener('dragleave', function(e) {
+                if (!e.currentTarget.contains(e.relatedTarget)) {
+                    var tempClone = dndHandler.draggedElement.cloneNode(true);
+                    var idx = $("#rotation").children().index(dndHandler.draggedElement);
+                    $(dndHandler.draggedElement).remove();
+                    updateRotationAfterIndex(idx);
+                    dndHandler.draggedElement = tempClone;
+                }
+                // this.className = 'dropper';
             });
-
-            var dndHandler = this;
 
             dropper.addEventListener('drop', function(e) {
                 var target = e.target,
@@ -44,32 +87,30 @@ actions.forEach(function (ac) {
                     target = target.parentNode;
                 }
 
-                target.className = 'dropper';
+                // target.className = 'dropper';
 
-                dndHandler.applyDragEvents(clonedElement);
-                dndHandler.applyClickToDeleteEvents(clonedElement);
-                addActionAtIndex(clonedElement, $("#rotation").children().length);
-                if (draggedElement.parentNode.id === "rotation")
-                	draggedElement.parentNode.removeChild(draggedElement);
+                dndHandler.applyRotationEvents(dndHandler.draggedElement);
+                // addActionAtIndex(clonedElement, $("#rotation").children().length);
+                // if (draggedElement.parentNode.id === "rotation")
+                // 	draggedElement.parentNode.removeChild(draggedElement);
             });
         },
 
-        applyClickEvents: function(element) {
-        	var dndHandler = this;
-        	element.addEventListener('click', function(e) {
-        		var target = e.target;
+        applyRotationEvents: function(element) {
+            element.draggable = true;
+
+            var dndHandler = this;
+
+            element.addEventListener('dragstart', function(e) {
+                var target = e.target;
                 while (target.parentNode.className.indexOf('draggable') != -1) {
                     target = target.parentNode;
                 }
-                var clonedElement = target.cloneNode(true);
-        		dndHandler.applyDragEvents(clonedElement);
-                dndHandler.applyClickToDeleteEvents(clonedElement);
-        		addActionAtIndex(clonedElement, $("#rotation").children().length);
-        	});
-        },
+                dndHandler.draggedElement = target;
+                // dndHandler.clone = dndHandler.draggedElement.cloneNode(true);
+                e.dataTransfer.setData('text/plain', '');
+            });
 
-        applyClickToDeleteEvents: function(element) {
-            var dndHandler = this;
             element.addEventListener('click', function(e) {
                 var target = e.target;
                 while (target.parentNode.className.indexOf('draggable') != -1) {
@@ -77,87 +118,51 @@ actions.forEach(function (ac) {
                 }
                 var idx = $("#rotation").children().index(target);
                 $(target).remove();
-                $("#rotation").children().filter(function(index) {return index >= idx;}).each(function(index) {
-                    var clonedElement = this.cloneNode(true);
-                    dndHandler.applyDragEvents(clonedElement);
-                    dndHandler.applyClickToDeleteEvents(clonedElement);
-                    addActionAtIndex(clonedElement, index + idx);
-                    this.remove();
-                });
+                updateRotationAfterIndex(idx);
             });
         }
     };
 
-    $(".draggable").each(function(index) { dndHandler.applyDragEvents(this); dndHandler.applyClickEvents(this); });
+    $(".draggable").each(function(index) { dndHandler.applyActionsEvents(this); });
     $(".dropper").each(function(index) { dndHandler.applyDropEvents(this); });
 })();
 
 var scale = 60;
 var gcd = 2.5;
 
-function addAction(element, time = 0) {
-	if (time == 0 && $("#rotation").children().length > 0) {
-        var timeGcd = 0;
-        if ($(element).hasClass("Weaponskill") && $("#rotation").children(".Weaponskill").length > 0)
-            timeGcd = (Number($("#rotation").children(".Weaponskill").last().attr("time")) * 100 + gcd * 100) / 100;
-        //animlock
-        var incr = defaultAnimLock;
-        var lastAc = actions.find(ac => $("#rotation").children().last().attr("name") === ac.name);
-        if (lastAc.hasOwnProperty("animLock"))
-            incr = lastAc.animLock;
-        // if (lastAc.name === "Delay") // TODO : Delay
-        //     incr = Number($("#rotation").children(".Weaponskill").last().attr("time"))
-        time = (Number($("#rotation").children().last().attr("time")) * 100 + incr * 100) / 100;
-        time = Math.max(time, timeGcd);
-	}
-	var position = time * scale;
-    var offset = 2;
-    if ($(element).hasClass("Ability"))
-        offset += 30;
-    var animLockHeight = scale * defaultAnimLock;
-    curAc = actions.find(ac => $(element).attr("name") === ac.name);
-    if (curAc.hasOwnProperty("animLock"))
-        animLockHeight = scale * curAc.animLock;
-    var addedElt = $(element).attr("time", `${time}`).css({"position": "absolute", "top": `${position}px`, "left": `${offset}px`, "height": `${animLockHeight}px`});
-    $("#rotation").append(addedElt);
-    addTimeUntil(time + 5);
-}
-
 function addActionAtIndex(element, idx) {
-    console.log(element);
-    console.log(idx);
     var time = 0;
     if (idx > 0) {
         var timeGcd = 0;
-        if ($(element).hasClass("Weaponskill") && $("#rotation").children().filter(function(index) {return index < idx && $(this).hasClass("Weaponskill");}).length > 0)
-            timeGcd = (Number($("#rotation").children().filter(function(index) {return index < idx && $(this).hasClass("Weaponskill");}).last().attr("time")) * 100 + gcd * 100) / 100;
-        console.log(timeGcd);
+        var gcds = $("#rotation").children().filter(function(index) {return index < idx && $(this).hasClass("Weaponskill");});
+        if ($(element).hasClass("Weaponskill") && gcds.length > 0)
+            timeGcd = (Number(gcds.last().attr("time")) * 100 + gcd * 100) / 100;
         //animlock
-        var incr = defaultAnimLock;
-        var lastAc = actions.find(ac => $("#rotation").children().filter(function(index) {return index < idx;}).last().attr("name") === ac.name);
-        if (lastAc.hasOwnProperty("animLock"))
-            incr = lastAc.animLock;
+        var previousAc = $("#rotation").children().filter(function(index) {return index < idx;}).last();
+        var incr = getAnimationLock(previousAc.attr("name"));
         // if (lastAc.name === "Delay") // TODO : Delay
         //     incr = Number($("#rotation").children(".Weaponskill").last().attr("time"))
-        time = (Number($("#rotation").children().filter(function(index) {return index < idx;}).last().attr("time")) * 100 + incr * 100) / 100;
-        console.log(time);
+        time = (Number(previousAc.attr("time")) * 100 + incr * 100) / 100;
         time = Math.max(time, timeGcd);
     }
 
     var position = time * scale;
-    var offset = 2;
+    var offset = 1;
     if ($(element).hasClass("Ability"))
         offset += 30;
-    var animLockHeight = scale * defaultAnimLock;
-    curAc = actions.find(ac => $(element).attr("name") === ac.name);
-    if (curAc.hasOwnProperty("animLock"))
-        animLockHeight = scale * curAc.animLock;
+    var animLockHeight = scale * getAnimationLock($(element).attr("name"));
     var addedElt = $(element).attr("time", `${time}`).css({"position": "absolute", "top": `${position}px`, "left": `${offset}px`, "height": `${animLockHeight}px`});
     if (idx > 0)
         $("#rotation").children().eq(idx-1).after(addedElt);
     else
         $("#rotation").prepend(addedElt);
     addTimeUntil(time + 5);
+}
+
+function updateRotationAfterIndex(idx) {
+    $("#rotation").children().filter(function(index) {return index >= idx;}).each(function(index) {
+        addActionAtIndex(this, index + idx);
+    });
 }
 
 function addTime(time) {
@@ -174,10 +179,34 @@ function addTimeUntil(time) {
     }
 }
 
-addTimeUntil(10);
+function getAnimationLock(actionName) {
+    var animLock = defaultAnimLock;
+    var action = actions.find(ac => actionName === ac.name);
+    if (action.hasOwnProperty("animLock"))
+        animLock = action.animLock;
+    return animLock;
+}
+
+addTimeUntil(20);
 
 $("#clearRotation").click(function(){
     $("#rotation").empty();
     $("#timeline").empty();
-    addTimeUntil(10);
+    addTimeUntil(20);
+});
+
+$("#opener").click(function(){
+    $("#rotation").empty();
+    $("#timeline").empty();
+    addTimeUntil(20);
+    var action = actions.find(ac => "Impulse Drive" === ac.name);
+    $("#actions").children(`#${action.group}`).children(`[name="${action.name}"]`).click();
+    action = actions.find(ac => "Life Surge" === ac.name);
+    $("#actions").children(`#${action.group}`).children(`[name="${action.name}"]`).click();
+    action = actions.find(ac => "Life Surge" === ac.name);
+    $("#actions").children(`#${action.group}`).children(`[name="${action.name}"]`).click();
+    action = actions.find(ac => "Impulse Drive" === ac.name);
+    $("#actions").children(`#${action.group}`).children(`[name="${action.name}"]`).click();
+    action = actions.find(ac => "Life Surge" === ac.name);
+    $("#actions").children(`#${action.group}`).children(`[name="${action.name}"]`).click();
 });
